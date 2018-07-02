a1b33fb30163f96cf763321726ee5c14e77fa6dd
Matheus Thomaz <matheusthomazt@gmail.com>
2018-07-02 13:36:31 -0300

    criado teste de atualizar e excluir

--- src/main/java/br/com/fabricadeprogramador/persistencia/jdbc/UsuarioDAO.java
+++ src/main/java/br/com/fabricadeprogramador/persistencia/jdbc/UsuarioDAO.java
@@ -29,4 +29,44 @@ public class UsuarioDAO {
 		}
 	}
 
+	public void alterar(Usuario usu) {
+     String sql = "update usuario set nome= ?, login= ?, senha= ? where id= ? ;";
+		
+		try {
+		PreparedStatement preparador = con.prepareStatement(sql);
+		preparador.setString(1, usu.getNome());
+		preparador.setString(2, usu.getLogin());
+		preparador.setString(3, usu.getSenha());
+		preparador.setInt(4, usu.getId()	);
+		//Executando o SQL no banco
+		preparador.execute();
+		//fechando o preparador
+		preparador.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	public void excluir(Usuario usu) {
+		
+		 String sql = "delete from usuario where id= ? ;";
+			
+			try {
+			PreparedStatement preparador = con.prepareStatement(sql);
+		    preparador.setInt(1, usu.getId());
+			//Executando o SQL no banco
+			preparador.execute();
+			//fechando o preparador
+			preparador.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		
+	}
+
 }
--- src/test/java/br/com/fabricadeprogramador/TesteUsuarioDAO.java
+++ src/test/java/br/com/fabricadeprogramador/TesteUsuarioDAO.java
@@ -4,20 +4,48 @@ import br.com.fabricadeprogramador.persistencia.entidade.Usuario;
 import br.com.fabricadeprogramador.persistencia.jdbc.UsuarioDAO;
 
 public class TesteUsuarioDAO {
-	
+
 	public static void main(String[] args) {
-		
+
+		testeexcluir();
+
+	}
+
+	public static void testeAlterar() {
+		Usuario usu = new Usuario();
+		usu.setId(1);
+		usu.setNome("Jaozao da silva");
+		usu.setLogin("jzaoss");
+		usu.setSenha("123345");
+
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		usuDAO.alterar(usu);
+
+		System.out.println("Alterado !");
+
+	}
+
+	public static void testeCadastrar() {
 		Usuario usu = new Usuario();
 		usu.setNome("Jaozao");
 		usu.setLogin("jzao");
 		usu.setSenha("123");
-		
+
 		UsuarioDAO usuDAO = new UsuarioDAO();
 		usuDAO.cadastrar(usu);
-		
+
 		System.out.println("cadastrado !");
-		
-		
+
+	}
+
+	public static void testeexcluir() {
+		Usuario usu = new Usuario();
+		usu.setId(1);
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		usuDAO.excluir(usu);
+
+		System.out.println("excluido !");
+
 	}
 
 }
